1aaf26a82c8b383f64b8026bdd7b302d34d9eec4
bloodnighttw <emails@bntw.dev>
2024-05-09 01:02:23 +0800

    comment test code.

--- src-tauri/src/auth.rs
+++ src-tauri/src/auth.rs
@@ -5,7 +5,7 @@ pub(crate) mod msa_auth;
 mod minecraft;
 
 #[tauri::command]
-pub async  fn msa_auth_init() -> String {
+pub async fn msa_auth_init() -> String {
     
     let ms_auth_flow = match msa_auth::MicrosoftAuthFlow::new(){
         Ok(flow) => flow,
@@ -38,34 +38,49 @@ pub async  fn msa_auth_init() -> String {
 
 }
 
-
-#[cfg(test)]
-mod tests {
-    use oauth2::StandardDeviceAuthorizationResponse;
-
-    use reqwest::Client;
-    use crate::auth::minecraft::MinecraftAuthorizationFlow;
-    use crate::auth::msa_auth;
-
-
-    #[test]
-    fn msa_test() {
-        let ms_auth_flow = msa_auth::MicrosoftAuthFlow::new().unwrap();
-        let detail:StandardDeviceAuthorizationResponse = ms_auth_flow.generate_msa_device_code_auth().unwrap();
-
-        println!(
-            "Open this URL in your browser:\n{}\nand enter the code: {}",
-            &detail.verification_uri().to_string(),
-            &detail.user_code().secret().to_string()
-        );
-
-        open::that(detail.verification_uri().to_string()).unwrap();
-
-        let mc_flow = MinecraftAuthorizationFlow::new(Client::new());
-        let (xbox_token,user_hash) = mc_flow.xbox_token(detail.user_code().secret()).unwrap();
-        let xbox_xsts_token = mc_flow.xbox_security_token(xbox_token).unwrap();
-        let mc_token = mc_flow.exchange_microsoft_token(user_hash,xbox_xsts_token).unwrap();
-
-        println!("{}",mc_token.access_token);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use oauth2::StandardDeviceAuthorizationResponse;
+// 
+//     use reqwest::Client;
+//     use serde_json::json;
+//     use crate::auth::minecraft::MinecraftAuthorizationFlow;
+//     use crate::auth::msa_auth;
+// 
+// 
+//     #[tokio::test]
+//     pub async fn msa_test() -> Result<()>{
+//         let ms_auth_flow = match msa_auth::MicrosoftAuthFlow::new(){
+//             Ok(flow) => flow,
+//             Err(e) => {
+//                 return json!({
+//                 "error": e.to_string()
+//             }).to_string()
+//             }
+//         };
+//         
+//         let detail:StandardDeviceAuthorizationResponse = match ms_auth_flow.generate_msa_device_code_auth().await{
+//             Ok(detail) => detail,
+//             Err(e) => {
+//                 return json!({
+//                 "error": e.to_string()
+//             }).to_string()
+//             }
+//         };
+// 
+//         println!(
+//             "Open this URL in your browser:\n{}\nand enter the code: {}",
+//             &detail.verification_uri().to_string(),
+//             &detail.user_code().secret().to_string()
+//         );
+// 
+//         open::that(detail.verification_uri().to_string()).unwrap();
+// 
+//         let mc_flow = MinecraftAuthorizationFlow::new(Client::new());
+//         let (xbox_token,user_hash) = mc_flow.xbox_token(detail.user_code().secret()).unwrap();
+//         let xbox_xsts_token = mc_flow.xbox_security_token(xbox_token).unwrap();
+//         let mc_token = mc_flow.exchange_microsoft_token(user_hash,xbox_xsts_token).unwrap();
+// 
+//         println!("{}",mc_token.access_token);
+//     }
+// }
