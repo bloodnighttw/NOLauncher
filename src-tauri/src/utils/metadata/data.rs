9530dfc95b7dd996ad837df6a043c372badfdf23
bloodnighttw <emails@bntw.dev>
2024-06-21 23:34:38 +0800

    Add PackageDetails and others.

--- src-tauri/src/utils/metadata/data.rs
+++ src-tauri/src/utils/metadata/data.rs
@@ -1,4 +1,6 @@
-use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use serde::{de, Deserialize, Deserializer, Serialize};
+use serde_json::{Value};
 
 #[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
 struct DependencyPackage {
@@ -26,12 +28,12 @@ pub struct VersionInfo{
     )]
     conflicts:Vec<DependencyPackage>,
     version:String,
-    volatile:bool
+    volatile: Option<bool>
 }
 
 #[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
 #[serde(rename_all = "camelCase")]
-pub struct VersionInfoList{
+pub struct PackageInfo {
     format_version:i32,
     name:String,
     uid:String,
@@ -39,21 +41,134 @@ pub struct VersionInfoList{
 }
 
 #[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
-#[serde(rename_all = "camelCase")]
 pub enum Platform{
+    #[serde(rename = "windows")]
     Windows,
+    #[serde(rename = "windows-arm64")]
     WindowsArm64,
+    #[serde(rename = "linux")]
     Linux,
+    #[serde(rename = "linux-arm32")]
     LinuxArm32,
+    #[serde(rename = "linux-arm64")]
     LinuxArm64,
-    MacOs,
+    #[serde(rename = "osx-arm64")]
     MacOsArm64,
     Unknown
 }
 
+#[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
+pub enum Action{
+    #[serde(rename = "allow")]
+    Allow,
+    #[serde(rename = "disallow")]
+    Disallow
+}
+
+fn os_processing<'de, D: Deserializer<'de>>(deserializer: D) -> anyhow::Result<Option<Platform>, D::Error> {
+
+    return Ok(match Value::deserialize(deserializer)?{
+        Value::Object(map) => {
+            if let Some(Value::String(osname)) = map.get("name"){
+
+                match osname.to_string().as_str(){
+                    "windows" => Some(Platform::Windows),
+                    "windows-arm64" => Some(Platform::WindowsArm64),
+                    "linux" => Some(Platform::Linux),
+                    "linux-arm32" => Some(Platform::LinuxArm32),
+                    "linux-arm64" => Some(Platform::LinuxArm64),
+                    "osx-arm64" => Some(Platform::MacOsArm64),
+                    _ => Some(Platform::Unknown)
+                }
+            } else {
+                None
+            }
+
+        }
+        _ => return Err(de::Error::custom("Invalid type"))
+    })
+}
+
+#[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
+pub struct Rule{
+    action:Action,
+    #[serde(deserialize_with = "os_processing", default)]
+    os:Option<Platform>
+}
+
+
+#[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
+pub struct Artifact{
+    pub url:String,
+    pub size:i64,
+    pub sha1:String,
+}
+
+#[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
+pub struct Download{
+    artifact:Option<Artifact>,
+    #[serde(skip_serializing_if = "HashMap::is_empty", default)]
+    classifiers:HashMap<String,Artifact>,
+}
+
+#[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
+pub struct Extract{
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
+    exclude:Vec<String>
+}
+
+
+#[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
+pub struct CommonLibrary {
+    name:String,
+    downloads:Download,
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
+    rules:Vec<Rule>,
+    extract:Option<Extract>,
+    #[serde(skip_serializing_if = "HashMap::is_empty", default)]
+    natives:HashMap<String,String>
+}
+
+#[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
+pub struct MavenLibrary{
+    name:String,
+    url:String,
+}
+
+#[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
+#[serde(untagged)]
+pub enum Library{
+    Common(CommonLibrary),
+    Maven(MavenLibrary)
+}
+
+
+#[derive(Debug,Clone,Serialize,Deserialize,PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct PackageDetails{
+    format_version: i32,
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
+    conflicts:Vec<DependencyPackage>,
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
+    requires:Vec<DependencyPackage>,
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
+    libraries:Vec<Library>,
+    name:String,
+    uid:String,
+    release_time:String,
+    #[serde(rename="type")]
+    type_:Option<String>, // neoforged hasn't this field
+    version:String,
+    volatile: Option<bool>,
+    main_class:Option<String>,
+    main_jar:Option<CommonLibrary>,
+    minecraft_arguments:Option<String>, 
+}
+
 #[cfg(test)]
 mod test{
-    use crate::utils::metadata::data::VersionInfoList;
+    use serde_json::json;
+    use crate::utils::metadata::data::{PackageDetails, PackageInfo, Rule};
 
     #[tokio::test]
     async fn test_version_info(){
@@ -70,8 +185,54 @@ mod test{
         ];
 
         for i in &test_api{
-            let res = reqwest::get(i.to_string()).await.unwrap().json::<VersionInfoList>().await.unwrap();
+            let _res = reqwest::get(i.to_string()).await.unwrap().json::<PackageInfo>().await.unwrap();
             //println!("{:?}",res);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_rule(){
+
+        let jsons = vec![
+            json!(
+                {
+                    "action": "allow"
+                }
+            ).to_string(),
+            json!(
+                {
+                    "action": "disallow",
+                    "os": {
+                        "name": "osx-arm64"
+                    }
+                }
+            ).to_string(),
+        ];
+
+        for i in &jsons{
+            let _res = serde_json::from_str::<Rule>(i).unwrap();
+        }
+    }
+
+    #[tokio::test]
+    async fn test_package_details(){
+
+        let test_api = vec![
+            "https://meta.prismlauncher.org/v1/org.lwjgl/2.9.3.json", // lwjgl 2.9.3
+            "https://meta.prismlauncher.org/v1/org.lwjgl3/3.3.1.json", // lwjgl 3.3.1
+            "https://meta.prismlauncher.org/v1/org.lwjgl3/3.2.1.json", // lwjgl 3.2.1, which has natives and classifiers
+            "https://meta.prismlauncher.org/v1/net.minecraft/1.21.json", // minecraft 1.21
+            "https://meta.prismlauncher.org/v1/net.minecraft/1.0.json", // minecraft 1.0
+            "https://meta.prismlauncher.org/v1/net.fabricmc.fabric-loader/0.15.1.json", // fabric-loader 0.15.1
+            "https://meta.prismlauncher.org/v1/net.fabricmc.intermediary/1.21.json", // fabric-intermediary 1.21
+            "https://meta.prismlauncher.org/v1/net.minecraftforge/51.0.16.json", // forge 51.0.16/1.21
+            "https://meta.prismlauncher.org/v1/net.neoforged/21.0.21-beta.json", // neoforged 21.0.21-beta
+            "https://meta.prismlauncher.org/v1/org.quiltmc.quilt-loader/0.26.1-beta.1.json", // quilt-loader 0.26.1-beta.1
+        ];
+
+        for i in &test_api{
+            let _res = reqwest::get(i.to_string()).await.unwrap().json::<PackageDetails>().await.unwrap();
+            // println!("{:?}",_res)
         }
     }
 }
