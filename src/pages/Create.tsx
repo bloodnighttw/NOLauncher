2cbc2dcced6642bf86186d2d1522fa7ecbcae70d
bloodnighttw <emails@bntw.dev>
2024-06-27 00:37:45 +0800

    Create Instance Window

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,51 +1,190 @@
 import {useEffect, useState} from "react";
-import {invoke} from "@tauri-apps/api/core";
-import {CenterView} from "../component/Compose.tsx";
 
-export function Create(){
+function NewInstance(){
+    const [version, setVersion] = useState<string>("Minecraft")
+    const [showRelease, setRelease] = useState(true);
+    const [showSnapshot, setSnapshot] = useState(false);
+    const [beta, setBeta] = useState(false);
+    const [alpha, setAlpha] = useState(false);
+    const [experiment, setExperiment] = useState(false);
 
-    const [version, setVersion] = useState<MinecraftVersionInfo | null>(null)
+    const loader=[
+        "Minecraft",
+        "Fabric",
+        "NeoForge",
+        "Forge",
+        "Quilt"
+    ]
 
-    useEffect(() => {
-        console.log("hi")
+    const typeFilter = (type:string|null) =>{
 
-        invoke<MinecraftVersionInfo>("list_versions").then((res) => {
-            setVersion(res)
-        })
+        switch (type) {
+            case "release":
+                return showRelease;
+            case "old_snapshot":
+            case "snapshot":
+                return showSnapshot;
+            case "old_beta":
+                return beta;
+            case "old_alpha":
+                return alpha;
+            case "experiment":
+                return experiment;
+        }
 
-    },[setVersion])
+        return false
+    }
+
+    const platform = "tab rounded-md duration-200 active:scale-90 hover:bg-base-200";
+    const platformActive = "tab rounded-md duration-200 bg-base-200 active:scale-90";
+
+    return <div className="py-1.5 w-full gap-2">
+        <div className="label">
+            <span className="label-text">Pick the platform you want to use:</span>
+        </div>
+        <div role="tablist" className="tabs bg-base-100 rounded-md p-0.5">
+            {loader.map((name) => {
+                return <div role="tab" className={version == name ? platformActive : platform}
+                            onClick={() => setVersion(name)}>{name}</div>
+            })}
+        </div>
+        <div className="label">
+            <span className="label-text">Select Minecraft Version</span>
+        </div>
+
+        <div className="flex flex-wrap gap-2">
+            <div className="w-40 px-2">
+                <label className="label cursor-pointer">
+                    <span className="label-text">Release</span>
+                    <input type="checkbox" className="toggle" defaultChecked onChange={(e) => {
+                        setRelease(e.target.checked)
+                    }}/>
+                </label>
+            </div>
+            <div className="w-40 px-2">
+                <label className="label cursor-pointer">
+                    <span className="label-text">Snapshot</span>
+                    <input type="checkbox" className="toggle" onChange={(e) => {
+                        setSnapshot(e.target.checked)
+                    }}/>
+                </label>
+            </div>
+            <div className="w-40 px-2">
+                <label className="label cursor-pointer">
+                    <span className="label-text">Experiment</span>
+                    <input type="checkbox" className="toggle" onChange={(e) => {
+                        setExperiment(e.target.checked)
+                    }}/>
+                </label>
+            </div>
+
+            <div className="w-40 px-2">
+                <label className="label cursor-pointer">
+                    <span className="label-text">Beta</span>
+                    <input type="checkbox" className="toggle" onChange={(e) => {
+                        setBeta(e.target.checked)
+                    }}/>
+                </label>
+            </div>
+
+            <div className="w-40 px-2">
+                <label className="label cursor-pointer">
+                    <span className="label-text">Alpha</span>
+                    <input type="checkbox" className="toggle" onChange={(e) => {
+                        setAlpha(e.target.checked)
+                    }}/>
+                </label>
+            </div>
+        </div>
+
+        <div className="label">
+            <span className="label-text">Select Minecraft Version</span>
+        </div>
+
+        <select className="select select-bordered w-full select-sm">
+            <option disabled selected>Selected Version</option>
+            <option value="minecraft">minecraft</option>
+            <option value="minecraft">fabric</option>
+            <option value="minecraft">forge</option>
+            <option value="minecraft">neoforge</option>
+        </select>
+        {
+            version != "Minecraft" ? <div className="duration-200">
+                <div className="label">
+                    <span className="label-text">Select {version} Version</span>
+                </div>
+
+                <select className="select select-bordered w-full select-sm">
+                    <option disabled selected>Selected Version</option>
+                    <option value="minecraft">minecraft</option>
+                    <option value="minecraft">fabric</option>
+                    <option value="minecraft">forge</option>
+                    <option value="minecraft">neoforge</option>
+                </select>
+            </div>: null
+        }
+
+
+        <div className="flex justify-end label">
+            <button className="btn btn-sm bg-base-100">Create!</button>
+        </div>
+
+    </div>
+}
+
+export function Create() {
+
+    const [tabIndex, setTabIndex] = useState(0)
+
+
+    const tab = "tab duration-200";
+    const tabActive = "tab tab-active duration-200";
+    const tabs = [
+        {long_name: "Create", component: <NewInstance/>},
+        {long_name: "Import", component: null},
+    ];
+
+
+    const tabClick = (index: number) => {
+        setTabIndex(index)
+    }
 
     return (
-            <CenterView>
-                <div role="tablist" className="tabs tabs-bordered">
-                    <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Tab 1"/>
-                    <div role="tabpanel" className="tab-content p-10">Tab content 1</div>
-
-                    <input
-                        type="radio"
-                        name="my_tabs_1"
-                        role="tab"
-                        className="tab"
-                        aria-label="Tab 2"
-                        defaultChecked/>
-                    <div role="tabpanel" className="tab-content p-10"  data-tauri-drag-region={true}>
-                        <button className="btn" onClick={() => document.getElementById('my_modal_2').showModal()}>open
-                            modal
-                        </button>
-                        <dialog id="my_modal_2" className="modal" >
-                            <div className="modal-box">
-                                <h3 className="font-bold text-lg">Hello!</h3>
-                                <p className="py-4">Press ESC key or click outside to close</p>
-                            </div>
-                            <form method="dialog" className="modal-backdrop">
-                                <button>close</button>
-                            </form>
-                        </dialog>
-                    </div>
-
-                    <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Tab 3"/>
-                    <div role="tabpanel" className="tab-content p-10">Tab content 3</div>
+        <div className="p-2">
+            <div className="w-full flex flex-row gap-2 p-2">
+                <div className="w-24 h-20">
+                    <img
+                        className="w-20 h-20 rounded-md object-cover"
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLqPCLMpN2yRL9noYNEuddweIC-Spud6jIuA&s"/>
                 </div>
-            </CenterView>
+                <div className="flex flex-col gap-2 w-full h-20">
+                    <input type="text" placeholder="Type Instance Name Here "
+                           className="input input-xs input-bordered w-full h-full"/>
+
+                    <select disabled className="select select-bordered w-full select-sm w-full h-full ">
+                        <option disabled selected>Tag is Coming soon......</option>
+                        <option value="minecraft">minecraft</option>
+                        <option value="minecraft">fabric</option>
+                        <option value="minecraft">forge</option>
+                        <option value="minecraft">neoforge</option>
+                    </select>
+                </div>
+
+
+            </div>
+            <div className="h-full space-y-2 p-2">
+                <div role="tablist" className="tabs tabs-bordered">
+                    {tabs.map((i, index) => (
+                        <div className={index == tabIndex ? tabActive : tab}
+                             onClick={() => tabClick(index)}>{i.long_name}</div>
+                    ))}
+                </div>
+
+                {tabs.map((i, index) => (
+                    index == tabIndex ? i.component : null
+                ))}
+
+            </div>
+        </div>
     )
 }
