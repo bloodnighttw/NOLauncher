0bce378acce2200835ef786a5c0389fea42e6a81
bloodnighttw <emails@bntw.dev>
2024-07-22 03:31:20 +0800

    Introducing Redux to project.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import { store } from "./state-hook/store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <BrowserRouter>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </BrowserRouter>
 );
