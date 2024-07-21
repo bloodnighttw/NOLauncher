fa4e21d38402cce4a284481a047bd76a4c6ed19d
bloodnighttw <emails@bntw.dev>
2024-07-22 00:41:00 +0800

    Rename function and file.

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,14 +1,13 @@
 
 import {CenterView} from "../../component/Compose.tsx";
-import {Devicecode} from "./Devicecode.tsx";
+import {Content} from "./Content.tsx";
 
 
 export function Auth() {
 
-    // https://flowbite.com/docs/components/stepper/https://flowbite.com/docs/components/stepper/
     return (
         <CenterView>
-            <Devicecode/>
+            <Content/>
         </CenterView>
 
     );
--- src/pages/Auth/Content.tsx
+++ src/pages/Auth/Content.tsx
@@ -53,7 +53,7 @@ const refresh_disable =
               d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"/>
     </svg>
 
-export function Devicecode() {
+export function Content() {
 
     const [devicecode, setDevicecode] = useState<DevicecodePayload | null>(null)
     const [message, setMessage] = useState("Fetching device code...")
@@ -186,7 +186,7 @@ export function Devicecode() {
             setStatus(Status.Unlock)
             setDevicecode(data)
             exchange.current = setTimeout(() => {
-                exchange_loop()
+                exchange_loop().catch(console.error)
             }, 5 * 1000)
 
         }).catch((err)=>handle_error(err,"Error while fetching devicecode"))
